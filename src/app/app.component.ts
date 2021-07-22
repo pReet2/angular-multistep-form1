0e54ce3deae6269177310f8c9f527211841ab34a
Preeti Gupta <preetiitzme@gmail.com>
2021-07-22 05:30:33 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'multistep-form1';
+  model = {}
+     
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  
+  constructor(private _formBuilder: FormBuilder) {}
+  
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      age: ['', Validators.required],
+      address: ['', Validators.required],
+      
+    });
+  }
+  
+  
+    submit() {
+      alert("Your form has been submitted. Thankyou!");
+    }
+    
+
+}
